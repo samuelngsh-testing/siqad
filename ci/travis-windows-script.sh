7fc54a2357c2860e5f4d15f5494eb00b9721e256
Samuel Ng <samuelngsh@gmail.com>
2020-01-12 01:19:29 -0800

    Minor fix

--- ci/travis-windows-script.sh
+++ ci/travis-windows-script.sh
@@ -11,15 +11,15 @@ echo `which cmake`
 cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${inst_dir}" -G "MinGW Makefiles" CMakeLists.txt
 mingw32-make -j8 install
 
-echo `which ldd`
+#echo `which ldd`
 
-echo `find .`
+#echo `find .`
 
 echo "SiQAD dependencies:"
-echo ldd "${bin_root}/siqad.exe"
+echo `ldd "${bin_root}/siqad.exe"`
 
 echo "SimAnneal dependencies:"
-echo ldd "${simanneal_bin_root}/simanneal.exe"
+echo `ldd "${simanneal_bin_root}/simanneal.exe"`
 
 cp `ldd "${bin_root}/siqad.exe" | grep -o -e '/mingw64.*dll' | sort -u` "${bin_root}"
 cp `ldd "${simanneal_bin_root}/simanneal.exe" | grep -o -e '/mingw64.*dll' | sort -u` "${simanneal_bin_root}"
