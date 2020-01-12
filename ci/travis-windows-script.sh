d3ad3caf44ddcf541c7cfe0e4a2993822e19f21b
Samuel Ng <samuelngsh@gmail.com>
2020-01-11 20:31:35 -0800

    Revert to non-static Qt and add dependencies for SimAnneal

--- ci/travis-windows-script.sh
+++ ci/travis-windows-script.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+inst_dir=build-w64
+simanneal_inst_dir="${inst_dir}/phys/simanneal"
+
 which cmake 
-cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=build-w64 -G "MinGW Makefiles" CMakeLists.txt
+cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${inst_dir}" -G "MinGW Makefiles" CMakeLists.txt
 mingw32-make -j8 install
 
+cp `ldd "${inst_dir}/siqad.exe" | grep -o -e '/mingw64.*dll' | sort -u` "${inst_dir}"
+cp `ldd "${simanneal_inst_dir}/simanneal.exe" | grep -o -e '/mingw64.*dll' | sort -u` "${inst_dir}"
+
 cd build-w64
-cp `ldd siqad.exe | grep -o -e '/mingw64.*dll' | sort -u` .
 windeployqt siqad.exe
 cd ..
 
