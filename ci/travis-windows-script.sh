b5e01cf286ab73830144dc2945cfada63c2d3398
Samuel Ng <samuelngsh@gmail.com>
2020-01-11 16:32:38 -0800

    Test MSYS build 12

--- ci/travis-windows-script.sh
+++ ci/travis-windows-script.sh
@@ -3,5 +3,5 @@
 set -e
 
 which cmake 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=build-w64 -G "MinGW Makefiles" CMakeLists.txt
+cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=build-w64 -G "MinGW Makefiles" CMakeLists.txt
 mingw32-make -j8 install
