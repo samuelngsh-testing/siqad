4a0ef3ea8a6bdfa8f950875c68c65f5efe71c136
Samuel Ng <samuelngsh@gmail.com>
2020-01-11 21:27:17 -0800

    Diagnostic prints

--- ci/travis-windows-script.sh
+++ ci/travis-windows-script.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-inst_dir=build-w64/release
+inst_dir=./build-w64/release
 simanneal_inst_dir="${inst_dir}/phys/simanneal"
 
-which cmake 
+echo `which cmake`
 cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${inst_dir}" -G "MinGW Makefiles" CMakeLists.txt
 mingw32-make -j8 install
+
+echo `which ldd`
+
+echo `find .`
 
 cp `ldd "${inst_dir}/siqad.exe" | grep -o -e '/mingw64.*dll' | sort -u` "${inst_dir}"
 cp `ldd "${simanneal_inst_dir}/simanneal.exe" | grep -o -e '/mingw64.*dll' | sort -u` "${inst_dir}"
