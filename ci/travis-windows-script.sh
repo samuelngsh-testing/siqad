2a1bfe4605989ad04634315e277104309657be5b
Samuel Ng <samuelngsh@gmail.com>
2020-01-11 20:05:16 -0800

    Copy dependencies

--- ci/travis-windows-script.sh
+++ ci/travis-windows-script.sh
@@ -6,4 +6,9 @@ which cmake
 cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=build-w64 -G "MinGW Makefiles" CMakeLists.txt
 mingw32-make -j8 install
 
+cd build-w64
+cp `ldd siqad.exe | grep -o -e '/mingw64.*dll' | sort -u` .
+windeployqt siqad.exe
+cd ..
+
 zip -r build-w64.zip build-w64
